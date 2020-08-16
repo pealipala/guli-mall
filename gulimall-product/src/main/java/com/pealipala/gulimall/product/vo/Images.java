0e88b132cb96fb00eafba57f8c7664caa9d7e65a
yechaoze <1029102110@qq.com>
2020-08-16 21:59:50 +0800

    保存商品信息

--- gulimall-product/src/main/java/com/pealipala/gulimall/product/vo/Images.java
+++ gulimall-product/src/main/java/com/pealipala/gulimall/product/vo/Images.java
@@ -3,28 +3,18 @@
   */
 package com.pealipala.gulimall.product.vo;
 
+import lombok.Data;
+
 /**
  * Auto-generated: 2020-08-16 15:12:40
  *
  * @author bejson.com (i@bejson.com)
  * @website http://www.bejson.com/java2pojo/
  */
+@Data
 public class Images {
 
     private String imgUrl;
     private int defaultImg;
-    public void setImgUrl(String imgUrl) {
-         this.imgUrl = imgUrl;
-     }
-     public String getImgUrl() {
-         return imgUrl;
-     }
-
-    public void setDefaultImg(int defaultImg) {
-         this.defaultImg = defaultImg;
-     }
-     public int getDefaultImg() {
-         return defaultImg;
-     }
 
 }
