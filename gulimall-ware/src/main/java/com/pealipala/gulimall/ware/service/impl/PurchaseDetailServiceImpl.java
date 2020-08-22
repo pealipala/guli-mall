cc84ed56fdac7eaacd7383e324d6c6caed54513f
yechaoze <1029102110@qq.com>
2020-08-22 18:55:51 +0800

    采购检索

--- gulimall-ware/src/main/java/com/pealipala/gulimall/ware/service/impl/PurchaseDetailServiceImpl.java
+++ gulimall-ware/src/main/java/com/pealipala/gulimall/ware/service/impl/PurchaseDetailServiceImpl.java
@@ -11,6 +11,7 @@ import com.pealipala.common.utils.Query;
 import com.pealipala.gulimall.ware.dao.PurchaseDetailDao;
 import com.pealipala.gulimall.ware.entity.PurchaseDetailEntity;
 import com.pealipala.gulimall.ware.service.PurchaseDetailService;
+import org.springframework.util.StringUtils;
 
 
 @Service("purchaseDetailService")
@@ -18,12 +19,31 @@ public class PurchaseDetailServiceImpl extends ServiceImpl<PurchaseDetailDao, Pu
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+        QueryWrapper<PurchaseDetailEntity> queryWrapper = new QueryWrapper<>();
+        String key = (String) params.get("key");
+        String status = (String) params.get("status");
+        String wareId = (String) params.get("wareId");
+        if (!StringUtils.isEmpty(key)){
+            queryWrapper.and((e)->{
+                e.eq("purchase",key).or().eq("sku_id",key);
+            });
+        }
+        if (!StringUtils.isEmpty(status)){
+            queryWrapper.eq("status",status);
+        }
+        if (!StringUtils.isEmpty(wareId)){
+            queryWrapper.eq("ware_id",wareId);
+        }
         IPage<PurchaseDetailEntity> page = this.page(
-                new Query<PurchaseDetailEntity>().getPage(params),
-                new QueryWrapper<PurchaseDetailEntity>()
+                new Query<PurchaseDetailEntity>().getPage(params),queryWrapper
         );
 
         return new PageUtils(page);
+    }
+
+    @Override
+    public PageUtils queryPageByCondition(Map<String, Object> params) {
+        return null;
     }
 
 }
--- gulimall-ware/src/main/java/com/pealipala/gulimall/ware/service/impl/WareSkuServiceImpl.java
+++ gulimall-ware/src/main/java/com/pealipala/gulimall/ware/service/impl/WareSkuServiceImpl.java
@@ -11,6 +11,7 @@ import com.pealipala.common.utils.Query;
 import com.pealipala.gulimall.ware.dao.WareSkuDao;
 import com.pealipala.gulimall.ware.entity.WareSkuEntity;
 import com.pealipala.gulimall.ware.service.WareSkuService;
+import org.springframework.util.StringUtils;
 
 
 @Service("wareSkuService")
@@ -18,9 +19,17 @@ public class WareSkuServiceImpl extends ServiceImpl<WareSkuDao, WareSkuEntity> i
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+        QueryWrapper<WareSkuEntity> queryWrapper = new QueryWrapper<>();
+        String skuId = (String) params.get("skuId");
+        String wareId = (String) params.get("wareId");
+        if (!StringUtils.isEmpty(skuId)){
+            queryWrapper.eq("sku_id",skuId);
+        }
+        if (!StringUtils.isEmpty(wareId)){
+            queryWrapper.eq("ware_id",wareId);
+        }
         IPage<WareSkuEntity> page = this.page(
-                new Query<WareSkuEntity>().getPage(params),
-                new QueryWrapper<WareSkuEntity>()
+                new Query<WareSkuEntity>().getPage(params),queryWrapper
         );
 
         return new PageUtils(page);
