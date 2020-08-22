0047d977219dcda2915e9987e151526a49ab232a
yechaoze <1029102110@qq.com>
2020-08-22 18:23:51 +0800

    保存商品信息

--- gulimall-ware/src/main/java/com/pealipala/gulimall/ware/GulimallWareApplication.java
+++ gulimall-ware/src/main/java/com/pealipala/gulimall/ware/GulimallWareApplication.java
@@ -6,12 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@EnableTransactionManagement
+
 @MapperScan("com.pealipala.gulimall.ware.dao")
+@EnableTransactionManagement
 @EnableDiscoveryClient
 @SpringBootApplication
 public class GulimallWareApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(GulimallWareApplication.class, args);
     }
